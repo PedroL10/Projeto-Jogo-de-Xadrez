f52e3b262bed97d116cbbcf57d31b05ff3c37580
Pedro Luis <pepluisgon01@gmail.com>
2022-04-25 09:41:32 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExcepetion.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessExcepetion extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExcepetion(String msg) {
+		super(msg);
+	}
+
+}
