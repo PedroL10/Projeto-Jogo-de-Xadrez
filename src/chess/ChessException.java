dbd155cf9e04f36ae1e863c20c29b029fe1f9789
Pedro Luis <pepluisgon01@gmail.com>
2022-04-25 20:33:05 -0300

    Handling exceptions and clearing screen

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -2,11 +2,11 @@ package chess;
 
 import boardgame.BoardException;
 
-public class ChessExcepetion extends BoardException {
+public class ChessException extends BoardException {
 
 	private static final long serialVersionUID = 1L;
 	
-	public ChessExcepetion(String msg) {
+	public ChessException(String msg) {
 		super(msg);
 	}
 
